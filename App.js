686271c413bb0979b25adfc0709626cb8f951497
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2023-09-11 17:58:53 +0200

    Add scroll View

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
   const [goalText, setGoalText] = useState("");
@@ -24,13 +31,15 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        {goals.map((goal) => (
-          <View style={styles.goalItem}>
-            <Text style={styles.goalText} key={goal}>
-              {goal}
-            </Text>
-          </View>
-        ))}
+        <ScrollView alwaysBounceVertical={false}>
+          {goals.map((goal) => (
+            <View style={styles.goalItem}>
+              <Text style={styles.goalText} key={goal}>
+                {goal}
+              </Text>
+            </View>
+          ))}
+        </ScrollView>
       </View>
     </View>
   );
@@ -59,7 +68,7 @@ const styles = StyleSheet.create({
     padding: 8,
   },
   goalsContainer: {
-    flex: 4,
+    flex: 5,
   },
   goalItem: {
     margin: 8,
