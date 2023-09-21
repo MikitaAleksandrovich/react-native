5bd7966d4baf00f6cdf0cb3c7bb1e4d5548ee88f
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2023-09-21 22:08:19 +0200

    Move to a separate component Goal Input

--- App.js
+++ App.js
@@ -8,29 +8,18 @@ import {
   FlatList,
 } from "react-native";
 import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
-  const [goalText, setGoalText] = useState("");
   const [goals, setGoals] = useState([]);
 
-  const goalInputHandler = (text) => {
-    setGoalText(text);
-  };
-
-  const addGoalHandler = () => {
+  const addGoalHandler = (goalText) => {
     setGoals([...goals, { text: goalText, id: Math.random().toString() }]);
   };
 
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Your course goal!"
-          onChangeText={goalInputHandler}
-        />
-        <Button title="Add Goal" onPress={addGoalHandler} />
-      </View>
+      <GoalInput onAddGoal={addGoalHandler} />
       <View style={styles.goalsContainer}>
         <FlatList
           data={goals}
@@ -48,22 +37,6 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 50,
     paddingHorizontal: 16,
-  },
-  inputContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: "#cccccc",
-    width: "70%",
-    marginRight: 8,
-    padding: 8,
   },
   goalsContainer: {
     flex: 5,
