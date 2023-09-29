9ae453852ef62311157aecf14e4ace9e454a479e
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2023-09-29 13:26:07 +0200

    Add pressable effect for android and ios

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,22 +1,29 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 
 const GoalItem = ({ itemText, onDeleteItem, id }) => (
-  <Pressable onPress={() => onDeleteItem(id)}>
-    <View key={itemText} style={styles.goalItem}>
+  <View key={itemText} style={styles.goalItem}>
+    <Pressable
+      android_ripple={{ color: "#210644" }}
+      onPress={() => onDeleteItem(id)}
+      style={({ pressed }) => pressed && styles.pressedItem}
+    >
       <Text style={styles.goalText}>{itemText}</Text>
-    </View>
-  </Pressable>
+    </Pressable>
+  </View>
 );
 
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
   },
+  pressedItem: {
+    opacity: 0.5,
+  },
   goalText: {
     color: "white",
+    padding: 8,
   },
 });
 
